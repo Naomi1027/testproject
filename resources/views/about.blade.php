ca1a6318586ddaae363ac9b76f209d49a65d4f08
Naomi1027 <149298368+Naomi1027@users.noreply.github.com>
2024-04-05 00:05:40 +0900

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,12 +11,12 @@
   <div class="container px-5 py-24 mx-auto flex flex-col">
     <div class="lg:w-4/6 mx-auto">
       <div class="rounded-lg h-64 overflow-hidden">
-        <img alt="content" class="object-cover object-center h-full w-full" src="../public/img/IMG_0370.jpeg">
+        <img alt="content" class="object-cover object-center h-full w-full" src="../storage/img/IMG_0370.jpeg">
       </div>
       <div class="flex flex-col sm:flex-row mt-10">
         <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
           <div class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-          <img alt="team" class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="../public/img/IMG_1571.PNG">
+          <img alt="team" class="w-20 h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="../storage/img/IMG_1571.PNG">
           <!-- <img fill="none" src="../public/pug.png" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
               <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
               <circle cx="12" cy="7" r="4"></circle> -->
